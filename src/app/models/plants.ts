00347703caa3c2960055b810e476b9bc11164936
Charles Witherspoon <charles.v.witherspoon@gmail.com>
2021-11-07 19:13:16 -0500

    Added plant ID

--- src/app/models/plants.ts
+++ src/app/models/plants.ts
@@ -4,28 +4,34 @@ export interface GrowthData {
 }
 
 export interface Plant {
+    id: number,
     name: string,
     growthData: GrowthData[]
 }
 
 export const MOCK_PLANTS: Plant[] = [
     {
+        id: 1,
         name: "Plant A",
         growthData: [],
     },
     {
+        id: 2,
         name: "Plant B",
         growthData: [],
     },
     {
+        id: 3,
         name: "Plant C",
         growthData: [],
     },
     {
+        id: 4,
         name: "Plant D",
         growthData: [],
     },
     {
+        id: 5,
         name: "Plant E",
         growthData: [],
     },
